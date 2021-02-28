837c8925bd73fbfb6daddb00a781232092e685fb
Joeri Jurion <51537901+j-jurion@users.noreply.github.com>
2021-02-28 18:07:22 +0100

    Full BLE Functionality, able to advertize and pair. When paired, sends out data.

---
+++ README.md
@@ -0,0 +1,12 @@
+# BLE Sense
+
+This is Arduino software, written for an Arduino Nano 33 IoT with a moisture sensor and a temperature sensor.
+
+In its initial state, the device is advertising. When a device is paired, it starts to send out data.
+
+Parts:
+- Arduino Nano 33 IoT
+- 9V battery
+- Moisture sensor
+- Temperature sensor
+
---
+++ sensor-ble/sensor-ble.ino
@@ -0,0 +1,114 @@
+//Development
+const bool develmode = true;
+
+
+//Identification
+#include <ArduinoUniqueID.h>
+String id = "";
+
+//BLE
+#include <ArduinoBLE.h>
+BLEService senseService("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLECharacteristic moistureChar("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 1);
+
+//Sensing
+  //Moisture
+const int sensorMoisturePin = A0;
+const int dryVal = 880;
+const int dryValNano = 700;
+const int dryValNano10M = 485;
+const int wetVal = 674;
+const int wetValNano = 230;
+const int wetValNano10M = 110;
+  //Temperature
+const int sensorTemperaturePin = A1;
+
+
+void setup() {
+  //Idenitifcation
+  id = readID();
+
+  //BLE
+  Serial.begin(9600);
+  while (!Serial);
+  pinMode(LED_BUILTIN, OUTPUT);
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+  BLE.setLocalName("BLE Sense");
+  BLE.setAdvertisedService(senseService);
+  senseService.addCharacteristic(moistureChar);
+  BLE.addService(senseService);
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  //BLE
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, HIGH);
+
+  while (central.connected()) {
+    //Sensing
+      //Moisture
+    int sensorMoistureVal = analogRead(sensorMoisturePin);
+    if (develmode) {
+      Serial.print("Sensor Value Moisture: ");
+      Serial.println(sensorMoistureVal);
+    }
+    int moisture = map(sensorMoistureVal, dryValNano10M, wetValNano10M, 0, 100); 
+    if (moisture < 0) {moisture = 0;}
+    if (moisture > 100) {moisture = 100;}
+      //Temperature
+    int sensorTemperatureVal = analogRead(sensorTemperaturePin);
+    float voltageTemp = (sensorTemperatureVal/1024.0) * 5.0;
+    float temperature = (voltageTemp - .5) * 100;
+    if (develmode) {
+      Serial.print("Sensor Value Temperature: ");
+      Serial.println(sensorTemperatureVal);
+      Serial.print(" Volts: ");
+      Serial.println(voltageTemp);
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+    }
+    
+
+    Serial.println();
+    printID();
+    Serial.print("Moisture: ");
+    Serial.println(moisture);
+    //Serial.println(moisture, HEX);
+    moisture = moisture, HEX;
+    moistureChar.writeValue((byte)moisture);
+    Serial.println();
+    delay(1000);
+
+    
+  }
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("Disconnected from central: ");
+  Serial.println(central.address());
+
+  //Serial.println();
+  delay(1000);
+  
+}
+
+
+//Identification
+String readID() {
+  String id = "";
+  for(size_t i = 0; i < UniqueIDsize; i++)
+    id = id + UniqueID[i];
+  return id;
+}
+void printID() {
+  Serial.print("ID: ");
+  Serial.println(id);
+}
